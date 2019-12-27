2821d1861080c869f64bdbcc37d0861b13983cc4
Michael Lambert <michael@alphageek.com.au>
2019-12-27 11:08:52 +1100

    Improve Javascript syntax

--- index.js
+++ index.js
@@ -1,38 +1,42 @@
-const permalinkFilter = (data) => {
+const defaultPermalinkFilter = require('hexo/lib/plugins/filter/post_permalink');
+
+const newPermalinkFilter = (data) => {
+    return data;
+};
+
+const postPermalink = (data) => {
+    if (typeof data.custompermalink !== 'undefined')
+    {
+        // Unregister the default permalink filter
+        hexo.extend.filter.unregister(
+            'post_permalink',
+            defaultPermalinkFilter
+        );
+        // Register the custom permalink filter
+        hexo.extend.filter.register(
+            'post_permalink',
+            newPermalinkFilter
+        );
+
+        return data.custompermalink;
+    }
+
+    // Unregister the custom permalink filter
+    hexo.extend.filter.unregister(
+        'post_permalink',
+        newPermalinkFilter
+    );
+    // Register the default permalink filter
+    hexo.extend.filter.register(
+        'post_permalink',
+        defaultPermalinkFilter
+    );
+
     return data;
 };
 
 hexo.extend.filter.register(
     'post_permalink',
-    function(data) {
-        if (typeof data.custompermalink !== 'undefined')
-        {
-            // Unregister the default permalink filter
-            hexo.extend.filter.unregister(
-                'post_permalink',
-                require('hexo/lib/plugins/filter/post_permalink')
-            );
-            // Register the custom permalink filter
-            hexo.extend.filter.register(
-                'post_permalink',
-                permalinkFilter
-            );
-
-            return data.custompermalink;
-        }
-
-        // Unregister the custom permalink filter
-        hexo.extend.filter.unregister(
-            'post_permalink',
-            permalinkFilter
-        );
-        // Register the default permalink filter
-        hexo.extend.filter.register(
-            'post_permalink',
-            require('hexo/lib/plugins/filter/post_permalink')
-        );
-
-        return data;
-    },
+    postPermalink,
     9
 );
