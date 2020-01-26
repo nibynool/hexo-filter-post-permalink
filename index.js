74738b53c3ee0af803ddb9ba77848cc3614ff0c5
Michael Lambert <michael@alphageek.com.au>
2020-01-26 11:38:39 +1100

    Always unregister filters then add required filter

--- index.js
+++ index.js
@@ -1,37 +1,35 @@
 const defaultPermalinkFilter = require('hexo/lib/plugins/filter/post_permalink');
 
 const newPermalinkFilter = (data) => {
-    return data;
+    return data.custompermalink;
 };
 
 const postPermalink = (data) => {
-    if (typeof data.custompermalink !== 'undefined')
-    {
-        // Unregister the default permalink filter
-        hexo.extend.filter.unregister(
-            'post_permalink',
-            defaultPermalinkFilter
-        );
-        // Register the custom permalink filter
-        hexo.extend.filter.register(
-            'post_permalink',
-            newPermalinkFilter
-        );
-
-        return data.custompermalink;
-    }
-
+    // Unregister the default permalink filter
+    hexo.extend.filter.unregister(
+        'post_permalink',
+        defaultPermalinkFilter
+    );
     // Unregister the custom permalink filter
     hexo.extend.filter.unregister(
         'post_permalink',
         newPermalinkFilter
     );
-    // Register the default permalink filter
-    hexo.extend.filter.register(
-        'post_permalink',
-        defaultPermalinkFilter
-    );
 
+    if (typeof data.custompermalink !== 'undefined')
+    {
+        // Register the custom permalink filter
+        hexo.extend.filter.register(
+            'post_permalink',
+            newPermalinkFilter
+        );
+    } else {
+        // Register the default permalink filter
+        hexo.extend.filter.register(
+            'post_permalink',
+            defaultPermalinkFilter
+        );
+    }
     return data;
 };
 
